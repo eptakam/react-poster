5997c39891fca64267ac52caf6dda0539de82aba
eptakam <emmataks@gmail.com>
2024-06-06 08:58:33 -0300

    Add styling using CSS file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 // ceci est un composant react
-
 import Post from "./components/Post";
 
 // un composant react est une fonction qui retourne du jsx
 function App() {
-  return <Post />;
+  return (
+    <main>
+      <Post author="Emmanuel" body="React.js is awesome!" />
+      <Post author="Paul" body="Check out the full course!" />
+    </main>
+  );
 }
 
 export default App
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,27 @@
-function Post(){
+// const names = ['Emmanuel', 'Paul'];
+
+// function Post(){
+//   const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+  
+//   return (
+//     <div>
+//       <p>{chosenName}</p>
+//       <p>React.js is awesome!</p>
+//     </div>
+//   );
+// }
+
+// export default Post;
+
+// rendre le composant dynamique
+import classes from './Post.module.css';
+
+function Post(props){
   return (
-    <div>
-      <p>Emmanuel</p>
-      <p>React.js is awesome!</p>
+    <div className={classes.post}>
+      <p className={classes.author}>{props.author}</p>
+      <p className={classes.text}>{props.body}</p>
     </div>
   );
 }
-
 export default Post;
