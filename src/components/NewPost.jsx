ec3645c8a4d723163f14211bbeeb5967f16fcc21
eptakam <emmataks@gmail.com>
2024-06-06 12:33:27 -0300

    Add event listener and useState (a react hook).

---
+++ src/components/NewPost.jsx
@@ -0,0 +1,20 @@
+import classes from './NewPost.module.css'
+
+function NewPost(props) {
+
+  return (
+    <form className={classes.form}>
+      <p>
+        <label htmlFor="body">Text</label>
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
+        {/* onChange se chargera d'appeler addeventlistener en arriere plan pour ecouter chaque changement dans la zone de texte le body qui se trouve dans PostsList en occurrence*/}
+      </p>
+      <p>
+        <label htmlFor="name">Your name</label>
+        <input type="text" id="name"required onChange={props.onAuthorChange} />
+      </p>
+    </form>
+  );
+}
+
+export default NewPost;
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,44 @@
+// importer le react hook useState
+// useState : is a React Hook
+// React Hook : is a js function that lets you use state and other React features in function components
+// they all begin by use and they most only execute in a React component or a custom Hook
+// useState : is a function that returns an array with 2 elements
+import { useState } from 'react';
+
+import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostsList.module.css';
 
 function PostsList () {
+  /*
+  Note: useState() returns an array with 2 elements:
+    stateData[0] // the current state value
+    stateData[1] // the function that lets you update the state value (setter function)  
+  */
+
+  // initialiser l'etat enteredBody avec une chaine de caractere vide
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  // fonction a passer en parametre de l'evenement onChange
+  function bodyChangeHandler(event) {
+    // mettre a jour l'etat enteredBody avec la valeur de l'input
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    // mettre a jour l'etat enteredBody avec la valeur de l'input
+    setEnteredAuthor(event.target.value);
+  }
+
   return(
-    <ul className={classes.posts}>
-        <Post author="Emmanuel" body="React.js is awesome!" />
+    <>
+      <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Paul" body="Check out the full course!" />
-    </ul>
+      </ul>
+    </>
   );
 }
 
