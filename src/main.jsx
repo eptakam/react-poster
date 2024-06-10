e69763bab519bcd4265b91692046e06df0e6df9b
eptakam <emmataks@gmail.com>
2024-06-10 16:17:23 -0300

    Adding routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NewPost from './components/NewPost.jsx';
+
+/*
+  Nous allons ajouter des urls pour: la page d'acceuil, un nouveau post, les details d'un post existant
+*/
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },  // composant react de la main page
+  { path: '/create-post', element: <NewPost />},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // l'element ayant pour id 'root' se trouve dans le fichier public/index.html
   
   // dire a react que le contenu rendu est en mode strict et doit etre afficher
   <React.StrictMode>
-    <App />   {/*   composant react */}
+    
+    <RouterProvider router={ router }/>   {/* router props: permet de configurer le RouterProvider pour qu'il fonctionne normalement cad qu'il sache l'url correspondant a chaque composant */}
   </React.StrictMode>,
 )
